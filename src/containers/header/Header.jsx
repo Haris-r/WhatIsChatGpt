b766eda107b90c4eb2d267f633688eb72b9d7324
Haris Rafiq <R1410548@virginmediao2.co.uk>
2025-07-10 12:41:29 +0100

    complete

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './header.css';  
+import people from '../../images/people.png';
+import ai from '../../images/ai.png';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+const Header = () => {
+
+    return (
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">Let's Build Something Amazing with GPT-3 OpenAI</h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your Email Address"/>
+                    <button type="button">Get Started</button>
+                </div>
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="people"/>
+                    <p>1{getRandomInt(999) /*I will get to the back-end of this one day*/ } people requested access a visit in last 24 hours</p> 
+                </div>
+            </div>            
+             <div className="gpt3__header-image">
+                <img src={ai} alt="ai"/>
+            </div>                                                                                                                                                    
+        </div>
+    )
+}
+
+export default Header;
